4d29bc52dae266f61c9f171bd907fb05d7afb0e1
Aaron Morneau <aaron.s.morneau@gmail.com>
2024-07-09 13:01:04 -0400

    altering find

--- app/api/dweet/route.ts
+++ app/api/dweet/route.ts
@@ -4,7 +4,7 @@ import Dweet from "@models/Dweet";
 export const GET = async (request): Promise<Response> => {
     try{
         await connectToDB();
-        const dweets = await Dweet.find().exec();
+        const dweets = await Dweet.find({});
         return new Response(JSON.stringify(dweets), { status: 200 });
     } catch ( error ) {
         return new Response("Failed to retrieve dweets", { status: 500 });
